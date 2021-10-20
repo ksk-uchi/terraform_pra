577edf0a5c9fc2b34e0be01f137f71215ea15541
ksk-uchi <ksk.uchi+github@gmail.com>
2021-10-20 20:42:06 +0900

    main tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  shared_credentials_file = "$HOME/.aws/credentials" 
+  profile = "default"
+  region  = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id = aws_subnet.example.id
+  key_name = aws_key_pair.deployer.key_name
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
